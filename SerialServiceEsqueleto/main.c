fd838640e68750e54ac46c78ab5998f5d76ee397
Sergio Alberino <alberino@gmail.com>
2020-12-05 11:29:57 -0300

    avances

--- SerialServiceEsqueleto/main.c
+++ SerialServiceEsqueleto/main.c
@@ -13,30 +13,80 @@
 //******* Definición de constantes ********
 #define SERIAL_PORT	3 // depende del puerto en la placa
 #define SERIAL_BAUDRATE 115200
-//#define BUFFER_SIZE 10
-#define PUERTO_TCP	10000
+#define BUFFER_SIZE 10
+
+#define MSG_S_RECEIVE_MODEL ">SW:1,1\r\n"
+#define MSG_S_SEND_MODEL ">OUT:1,1\r\n"
+#define MSG_S_SEND_SIZE 10
+//#define MSG_S_SEND_SIZE strlen(MSG_S_SEND_MODEL)
+#define MSG_S_RECEIVE_SIZE 10
+//#define MSG_S_RECEIVE_SIZE strlen(MSG_S_RECEIVE_MODEL)
+
+#define TCP_PORT	10000
 #define ADDR_IP		"127.0.0.1"
-#define MSG_TYPE ">OUT:1,0\r\n"
-#define BUFFER_SIZE strlen(MSG_TYPE)
+
 
 //*********** Variablos globales ***********
 char Buffer[BUFFER_SIZE];
 char Buffer2[BUFFER_SIZE];
 int ret;
+int ret2;
 
 pthread_t h_thread,h_thread1;
-void* ret;
 volatile sig_atomic_t flag_fin;
 
-// Variables que se usan para la conexión tcp
 socklen_t addr_len;
 struct sockaddr_in clientaddr;
 struct sockaddr_in serveraddr;
-char buffer_tx[128],buffer_rx[128];
-char data_send[]=MSG_SERIAL_SND;
+char buffer_tx[128];
+char buffer_rx[128];
+char data_send[]=MSG_S_SEND_MODEL;
 int newfd;
-int n;
-int fd_s;
+int s;
+
+
+
+// ***********Threads ***************** 
+void* start_tcp(void* message)
+{
+	while(1){
+		// se hace accept() para recibir conexiones entrantes,
+		printf("Esperando por un cliente\n");
+		addr_len = sizeof(struct sockaddr_in);
+    	if ( (newfd = accept(s, (struct sockaddr *)&clientaddr, &addr_len)) == -1){
+	 		perror("Error en accept");
+	   		exit(1);
+		}
+
+		char ipClient[32];
+		inet_ntop(AF_INET, &(clientaddr.sin_addr), ipClient, sizeof(ipClient));
+		printf  ("Server:  %s\n",ipClient);
+
+		if( (ret2 = read(newfd,buffer_tx,128)) == -1 ){
+			perror("Error leyendo mensaje del socket");
+			// exit(1);
+		}
+		
+		buffer_tx[ret]=0;
+
+		if(ret2==BUFFER_SIZE){
+			serial_send(buffer_tx,ret2);
+			printf("%d bytes recibidos del socket %s\n",ret,buffer_tx);
+		}
+
+		while(ret2!=-1 && ret2!=0){
+			ret2 = read(newfd,buffer_tx,128);
+			if(ret==BUFFER_SIZE){
+				strcpy(data_send,buffer_tx);
+				printf ("%s",buffer_tx);
+				serial_send(data_send,ret2);
+				printf("%d bytes recibidos del socket %s\n",ret2,buffer_tx);
+			}
+		}
+		printf("Hubo un problema en al conexión\n");
+		close(newfd);
+	}
+}
 
 
 
@@ -50,6 +100,20 @@ int main(void)
         printf("Error opening serial port1: %d\n", ret);
         exit(1);
     }
+
+
+	// Cargamos datos de direccion de server
+    	bzero((char *) &serveraddr, sizeof(serveraddr));
+    	serveraddr.sin_family = AF_INET;
+    	serveraddr.sin_port = htons(4096);
+    	//serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    	if(inet_pton(AF_INET, ADDR_IP, &(serveraddr.sin_addr))<=0)
+    	{
+        	fprintf(stderr,"ERROR servidor invalido IP\r\n");
+        	return 1;
+    	}
+
+
 
 	printf("Inicio Serial Service\r\n");
 	printf("prende LED1 \r\n");
